5bcccf3e2bf9d3d06268c7c9775a6d151f147fba
weycy <wcy1015@126.com>
2022-11-24 11:31:36 +0800

    收集各个组件数据

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -15,7 +15,9 @@ type CategoryType = '-' | '+'
 
 const defaultFormData = {
   category: '-' as CategoryType,
-  note:'',
+  note: '',
+  amount: 0,
+  tags: [] as string[],
 };
 
 function Money() {
@@ -28,12 +30,19 @@ function Money() {
   };
   return (
     <MyLayout>
+      {selected.tags}  {selected.category} {selected.amount} {selected.note}
       <Category value={selected.category}
                 onChange={category => onChange({category})}/>
-      {selected.category === '-' ? <OutputTabs/> : <InputTabs/>}
+      {selected.category === '-' ?
+        <OutputTabs selected={selected.tags} onChange={tags => onChange({tags})}/> :
+        <InputTabs/>}
 
       <NumberPad noteValue={selected.note}
-      noteOnChange={note => onChange({note})}/>
+                 noteOnChange={note => onChange({note})
+                 }
+                 amountValue={selected.amount}
+                 amountOnChange={amount => onChange({amount})}
+      />
     </MyLayout>
   );
 }
