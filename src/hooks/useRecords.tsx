8194932474a9c1bd8debdc72f5309efb96abe03e
weycy <wcy1015@126.com>
2022-11-27 14:05:14 +0800

    表单验证

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -12,15 +12,23 @@ type RecordItem = {
 
 type NewRecordItem = Omit<RecordItem, 'createdAt'>;
 
-
 export const useRecords = () => {
   const [records, setRecords] = useState<RecordItem[]>([]);
   useEffect(() => {
     setRecords(JSON.parse(window.localStorage.getItem('records') || '[]'))
   }, [])
   const addRecord = (newRecord: NewRecordItem) => {
+    if(newRecord.amount <= 0){
+      return alert('请输入金额')
+      return false
+    }
+    if(newRecord.tagIds.length === 0){
+      return alert('请选择标签')
+      return false
+    }
     const record = {...newRecord, createdAt: (new Date()).toISOString()}
     setRecords([...records, record])
+    return true
   }
 
   useUpdate(() => {
